31de4cfac3411302fe22231d187c4cd0b2a72a79
Eric Wise <ewise82@gmail.com>
2020-10-30 15:44:19 -0700

    Add webpack and babel config files

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/Client/Components');
+const PUBLIC_DIR = path.join(__dirname, '/Public');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: PUBLIC_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
