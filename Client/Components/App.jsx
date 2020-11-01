cc9cdeabf1e375a68a553d6bfd72574fb63dd161
Eric Wise <ewise82@gmail.com>
2020-10-31 21:03:34 -0700

    Set up GET route

--- Client/Components/App.jsx
+++ Client/Components/App.jsx
@@ -1,6 +1,21 @@
+import axios from 'axios';
 import React, { useState, useEffect} from 'react';
 
 function App() {
+
+
+
+
+const checkWord = (word) => {
+    axios.get(`/api/words/${word}`)
+      .then((result) => {
+        // check result to see if entry was a word
+      })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+    
 return (
     <div>Hello from React</div>
 );
--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,14 @@
+const db = require('../Database/index.js');
 const express = require('express');
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.static(`${__dirname}/../Public`));
+
+app.get('/api/words/:word', (req, res) => {
+
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
