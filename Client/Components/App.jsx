3c5348bc74ee881ac02dadb9f2f87688b40b21a9
Eric Wise <ewise82@gmail.com>
2020-11-01 22:15:13 -0800

    Using circles instead of hexagons

--- Client/Components/App.jsx
+++ Client/Components/App.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useState, useEffect} from 'react';
+import './style.css';
 
 function App() {
 const [wordList, setWordList] = useState([]);
@@ -16,7 +17,41 @@ const checkWord = (word) => {
 }
     
 return (
-    <div>Hello from React</div>
+
+<div className="main-box">
+  
+  <div className="column">
+    <div className="circle-1">
+        <div className="circle">S</div> 
+    </div>
+    <div className="circle-2">
+        <div className="circle">P</div>
+    </div>
+  </div>
+
+  <div className="column">
+    <div className="circle-3">
+        <div className="circle">R</div>
+    </div>
+    <div className="circle-center">
+        <div className="circle-middle">E</div>
+    </div>
+    <div className="circle-5">
+        <div className="circle">A</div>
+    </div>
+  </div>
+
+  <div className="column">
+    <div className="circle-6">
+        <div className="circle">D</div>
+    </div>
+    <div className="circle-7">
+        <div className="circle">I</div>
+    </div>
+  </div>
+</div>
+
+    
 );
 }
 
--- Database/index.js
+++ Database/index.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 const mongoUri = 'mongodb://localhost/word';
 
-const db = mongoose.connect(mongoUri, {useNewUrlParser: true});
+const db = mongoose.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = db;
